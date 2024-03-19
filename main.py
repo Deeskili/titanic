28bb0f3d954cce59330f1aecb09a2344529fc86c
Deeskili <Caydenyslsy@gmail.com>
2024-03-19 11:54:05 -0700

    titanic :O

--- main.py
+++ main.py
@@ -1,70 +1,145 @@
-import threading
+import pandas as pd
+import seaborn as sns
+from flask import Flask, request, jsonify
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from flask_cors import CORS
 
-# import "packages" from flask
-from flask import render_template,request  # import render_template from "public" flask libraries
-from flask.cli import AppGroup
+app = Flask(__name__)
+CORS(app)
+
+# Load the titanic dataset
+titanic_data = sns.load_dataset('titanic')
+print("Titanic Data")
+print(titanic_data[['survived', 'pclass', 'sex', 'age', 'sibsp', 'parch', 'fare', 'embarked', 'alone']])
+
+# Preprocess the data
+td = titanic_data.copy()
+td.drop(['alive', 'who', 'adult_male', 'class', 'embark_town', 'deck'], axis=1, inplace=True)
+td.dropna(inplace=True)  # drop rows with at least one missing value, after dropping unuseful columns
+td['sex'] = td['sex'].apply(lambda x: 1 if x == 'male' else 0)
+td['alone'] = td['alone'].apply(lambda x: 1 if x else 0)
+
+# Encode categorical variables
+enc = OneHotEncoder(handle_unknown='ignore')
+enc.fit(td[['embarked']])
+onehot = enc.transform(td[['embarked']]).toarray()
+cols = ['embarked_' + val for val in enc.categories_[0]]
+td[cols] = pd.DataFrame(onehot) 
+td.drop(['embarked'], axis=1, inplace=True)
+td.dropna(inplace=True)  # drop rows with at least one missing value, after preparing the data
+print(td.columns)
+print(td)
+
+print(td.select_dtypes(include=['number']).median())
+# Calculate the mean of numeric columns only
+numeric_columns = titanic_data.select_dtypes(include=['number'])
+mean_numeric_columns = numeric_columns.mean()
+print(mean_numeric_columns)
+
+print(td.query("survived == 1").mean())
+print("maximums for survivors")
+print(td.query("survived == 1").max())
+print()
+print("minimums for survivors")
+print(td.query("survived == 1").min())
+
+# Build distinct data frames on survived column
+X = td.drop('survived', axis=1)  # all except 'survived'
+y = td['survived']  # only 'survived'
+
+# Split arrays in random train 70%, random test 30%, using stratified sampling (same proportion of survived in both sets) and a fixed random state (42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Train a decision tree classifier
+dt = DecisionTreeClassifier()
+dt.fit(X_train, y_train)
+
+# Test the model
+y_pred = dt.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print('DecisionTreeClassifier Accuracy: {:.2%}'.format(accuracy))
+
+# Train a logistic regression model
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+
+# Test the model
+y_pred = logreg.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print('LogisticRegression Accuracy: {:.2%}'.format(accuracy))
+
+# Define the Flask app
+app = Flask(__name__)
+
+# Define the TitanicRegression class
+class TitanicRegression:
+    def __init__(self):
+        self.encoder = None
+        self.logreg = None
+
+    def initTitanic(self):
+        titanic_data = sns.load_dataset('titanic')
+        X = titanic_data.drop('survived', axis=1)
+        y = titanic_data['survived']
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+        # Initialize the encoder
+        self.encoder = OneHotEncoder(handle_unknown='ignore')
+        self.X_train = self.encoder.fit_transform(X_train)
+        self.X_test = self.encoder.transform(X_test)
+
+        # Train a logistic regression model
+        self.logreg = LogisticRegression()
+        self.logreg.fit(self.X_train, y_train)
+
+    def predictSurvival(self, passenger):
+        passenger_df = pd.DataFrame([passenger], columns=['name','pclass', 'sex', 'age', 'sibsp', 'parch', 'fare', 'embarked', 'alone'])
+
+        # Preprocess the passenger data
+        passenger_encoded = self.encoder.transform(passenger_df)
+
+        predict = self.logreg.predict(passenger_encoded)
+        return predict
 
 
-# import "packages" from "this" project
-from __init__ import app, db, cors  # Definitions initialization
+# Initialize the Titanic model
+titanic_model = TitanicRegression()
+titanic_model.initTitanic()
+
+# Define the API endpoint for prediction
+@app.route('/api/predict', methods=['POST'])
+def predict():
+    # Get the passenger data from the request
+    json_data = request.get_json()
 
 
-# setup APIs
-from api.covid import covid_api # Blueprint import api definition
-from api.joke import joke_api # Blueprint import api definition
-from api.user import user_api # Blueprint import api definition
-from api.player import player_api
-# database migrations
-from model.users import initUsers
-from model.players import initPlayers
+    name = json_data.get('name')
+    pclass = json_data.get('pclass')
+    sex = json_data.get('sex')
+    age = json_data.get('age')
+    sibsp = json_data.get('sibsp')
+    parch = json_data.get('parch')
+    fare = json_data.get('fare')
+    embarked = json_data.get('embarked')
+    alone = json_data.get('alone')
 
-# setup App pages
-from projects.projects import app_projects # Blueprint directory import projects definition
+    if alone == "true":
+        alone = True
+    elif alone == "false":
+        alone = False
 
+    data_list = [name, pclass, sex, age, sibsp, parch, fare, embarked, alone]    
 
-# Initialize the SQLAlchemy object to work with the Flask app instance
-db.init_app(app)
+    # Use the model to predict survival
+    response = titanic_model.predictSurvival(data_list)
 
-# register URIs
-app.register_blueprint(joke_api) # register api routes
-app.register_blueprint(covid_api) # register api routes
-app.register_blueprint(user_api) # register api routes
-app.register_blueprint(player_api)
-app.register_blueprint(app_projects) # register app pages
+    # Return the response as JSON
+    return jsonify(response)
 
-@app.errorhandler(404)  # catch for URL not found
-def page_not_found(e):
-    # note that we set the 404 status explicitly
-    return render_template('404.html'), 404
-
-@app.route('/')  # connects default URL to index() function
-def index():
-    return render_template("index.html")
-
-@app.route('/table/')  # connects /stub/ URL to stub() function
-def table():
-    return render_template("table.html")
-
-@app.before_request
-def before_request():
-    # Check if the request came from a specific origin
-    allowed_origin = request.headers.get('Origin')
-    if allowed_origin in ['http://localhost:4100', 'http://127.0.0.1:4100', 'https://nighthawkcoders.github.io']:
-        cors._origins = allowed_origin
-
-# Create an AppGroup for custom commands
-custom_cli = AppGroup('custom', help='Custom commands')
-
-# Define a command to generate data
-@custom_cli.command('generate_data')
-def generate_data():
-    initUsers()
-    initPlayers()
-
-# Register the custom command group with the Flask application
-app.cli.add_command(custom_cli)
-        
-# this runs the application on the development server
 if __name__ == "__main__":
-    # change name for testing
+    # Run the Flask app
     app.run(debug=True, host="0.0.0.0", port="8086")
